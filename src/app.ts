e0d69b37dd297fb3c2fc72aaaffbae842772ea61
kchn9 <mat.kuchnia@gmail.com>
2022-12-07 15:18:08 +0100

    Init db, middleware, controllers & error handle

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,57 @@
+import errorMiddleware from "@/middleware/error.middleware";
+import Controller from "@/utils/interfaces/controller.interface";
+import compression from "compression";
+import cors from "cors";
 import express, { Application } from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import pino from "pino-http";
 
 class App {
     public express: Application;
     public port: number;
 
-    constructor(port: number) {
+    constructor(controllers: Controller[], port: number) {
         this.express = express();
         this.port = port;
+
+        this.initializeDatabaseConnection();
+        this.initializeMiddleware();
+        this.initializeControllers(controllers);
+        this.initializeErrorHandling();
+    }
+
+    private initializeDatabaseConnection(): void {
+        const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST } = process.env;
+        mongoose.connect(
+            `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}`
+        );
+    }
+
+    private initializeMiddleware(): void {
+        this.express.use(helmet());
+        this.express.use(cors());
+        this.express.use(pino());
+        this.express.use(express.json());
+        this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(compression());
+    }
+
+    private initializeControllers(controllers: Controller[]): void {
+        controllers.forEach((controller: Controller) => {
+            this.express.use(`/api`, controller.router);
+        });
+    }
+
+    private initializeErrorHandling(): void {
+        this.express.use(errorMiddleware);
+    }
+
+    public listen(): void {
+        this.express.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}`);
+        });
     }
 }
+
+export default App;
